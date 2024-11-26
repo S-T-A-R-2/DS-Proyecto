1926bd78838cab3c0269f3e3e48b7798b489bed2
OscarObando <osobando@estudiantec.cr>
2024-11-25 23:12:58 -0600

    coso de strategy

--- Sistema-Canje/backend/src/controllers/exchange-record-controller.ts
+++ Sistema-Canje/backend/src/controllers/exchange-record-controller.ts
@@ -1,6 +1,5 @@
-import ExchangeRecord from '../models/exchange-record-model';
-import {ExchangeRecordClass} from '../classes/ExchangeRecord'
-import CronologicalStrategy from '../classes/CronologicalStrategy'
+import {ExchangeRecordClass} from '../classes/ExchangeRecord';
+import DetailStrategy from '../classes/DetailStrategy';
 
 class ExchangeRecordController {
 
@@ -16,28 +15,18 @@ class ExchangeRecordController {
 
   public async getAllExchanges(){
     if (this.exchanges.length == 0){
-        const exchanges = await ExchangeRecord.find({});
-        for (let exchange of exchanges) {
-            const object = new ExchangeRecordClass(
-                exchange.number,
-                exchange.username,
-                exchange.medicine,
-                exchange.date,
-                exchange.pharmacy,
-                exchange.invoicesUsed
-            );
-            this.exchanges.push(object);
-        }
+      let detail = new DetailStrategy();
+      const a = await detail.execute()
+      this.exchanges = a.sort((a, b) => a.date.localeCompare(b.date));
+
     }
     return this.exchanges;
   }
 
   public getExchangesByUser(username:string){
-      let crono = new CronologicalStrategy();  
-      let ordered =  crono.execute(this.exchanges);
-      let result = ordered.filter((ex) => ex.username === username);
+      let result = this.exchanges.filter((ex) => ex.username === username);
 
-      return result;
+      return result.sort((a, b) => a.date.localeCompare(b.date));
       
   }
 }
