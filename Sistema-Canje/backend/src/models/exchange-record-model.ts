66325938c4a910419d190faae29e1769b5594169
OscarObando <126714980+OscarObando@users.noreply.github.com>
2024-11-25 18:01:39 -0600

    Add files via upload

---
+++ Sistema-Canje/backend/src/models/exchange-record-model.ts
@@ -0,0 +1,34 @@
+import mongoose, {model, Document, Schema} from 'mongoose'
+
+const ExchangeRecord = new Schema({
+    number: {
+      type: Number,
+      required: true,
+      unique:true
+    },  
+    username: {
+        type: String,
+        required: true
+    },
+    medicine: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    pharmacy: {
+      type:String,
+      required:true
+    },
+    invoicesUsed {
+      type: [Number],
+      required:true
+    }
+ 
+});
+export default model('ExchangeRecord', ExchangeRecord);
+
+
+
